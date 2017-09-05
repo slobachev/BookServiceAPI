94a42e51f8178dce44594dfbc7194ba49fe082ff
Sergey <seruy5432@gmail.com>
2017-09-05 21:25:51 +0300

    Implement book service

--- BookServiceAPI/Services/BookService.cs
+++ BookServiceAPI/Services/BookService.cs
@@ -7,24 +7,39 @@ namespace BookServiceAPI.Services
 {
     public class BookService : IBookService
     {
+        private readonly List<Book> books;
+
+        public BookService()
+        {
+            books = new List<Book>
+            {
+                new Book { Id = 1, Title = "Les Misérables", ReleaseYear = 1862, Summary = "Les Misérables is a French historical novel by Victor Hugo, first published in 1862, that is considered one of the greatest novels of the 19th century." },
+                new Book { Id = 2, Title = "Ulysses", ReleaseYear = 1922, Summary = "Ulysses is a modernist novel by Irish writer James Joyce." },
+                new Book { Id = 3, Title = "Julius Caesar", ReleaseYear = 1599, Summary = "The Tragedy of Julius Caesar is a tragedy by William Shakespeare, believed to have been written in 1599." }
+            };
+        }
         public List<Book> GetBooks()
         {
-            throw new NotImplementedException();
+            return books;
         }
 
         public Book GetBook(int id)
         {
-            throw new NotImplementedException();
+            return books.FirstOrDefault(item => item.Id == id);
         }
 
         public void AddBook(Book item)
         {
-            throw new NotImplementedException();
+            books.Add(item);
         }
 
         public void UpdateBook(Book item)
         {
-            throw new NotImplementedException();
+            var target = books.FirstOrDefault(t => t.Id == item.Id);
+
+            target.Title = item.Title;
+            target.ReleaseYear = item.ReleaseYear;
+            target.Summary = item.Summary;
         }
 
         public void DeleteBook(int id)
